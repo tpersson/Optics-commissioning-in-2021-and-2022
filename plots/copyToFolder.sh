92d2a8e658551a3cfbaaaf8e66a266179d7cd129
Tobias Hakan Bjorn Persson <tpersson@lxplus725.cern.ch>
2022-10-10 10:39:17 +0200

    Some cleanup.

--- plots/copyToFolder.sh
+++
@@ -1,2 +0,0 @@
-cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_05_10_30cm_global_corrections_and_rigid_waist_shift/plots/lhcb1_30cm_before_vs_after_arc_by_arc_coupling.pdf .
-cp -r /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022//COM_2022_05_02_squeeze_local_corrections/plots/* .
