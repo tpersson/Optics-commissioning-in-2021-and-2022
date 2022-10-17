7749c4a558c2c46d9712f4b9ee8720ab7d50a8e0
Tobias Hakan Bjorn Persson <tpersson@lxplus733.cern.ch>
2022-10-17 04:52:05 +0200

    Added the ramp plots.

---
+++ ramp/copyToFolder.sh
@@ -0,0 +1,4 @@
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_08_04_Ramp_Measurements/Beam1_rms_filter.pdf .
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_08_04_Ramp_Measurements/Beam1_peak_filter.pdf .
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_08_04_Ramp_Measurements/simulateMQTs/mqt_in_ramp_1900GeV_horizontal.pdf .
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_08_04_Ramp_Measurements/simulateMQTs/mqt_in_ramp_1900GeV_horizontal_zoom.pdf .
