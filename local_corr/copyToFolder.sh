ed04e76ea7c83e30c2ee017d20b0e02eba584387
Tobias Hakan Bjorn Persson <tpersson@cs-ccr-dev2.cern.ch>
2022-10-24 15:31:56 +0200

    Added plots for local corrections.

--- local_corr/copyToFolder.sh
+++ local_corr/copyToFolder.sh
@@ -1,2 +1,3 @@
 cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_05_10_30cm_global_corrections_and_rigid_waist_shift/plots/lhcb1_30cm_before_vs_after_arc_by_arc_coupling.pdf .
 cp -r /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022//COM_2022_05_02_squeeze_local_corrections/plots/* .
+cp /afs/cern.ch/eng/sl/lintrack/LHC_commissioning2022/COM_2022_05_02_squeeze_local_corrections/compare/*pdf .
