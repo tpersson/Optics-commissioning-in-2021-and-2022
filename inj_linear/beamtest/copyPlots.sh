7e18745ab5bf256a7cd359f127705518b8ed83ab
Tobias Hakan Bjorn Persson <tpersson@lxplus706.cern.ch>
2022-10-25 14:31:56 +0200

    Added files from the beam test in 2021.

---
+++ inj_linear/beamtest/copyPlots.sh
@@ -0,0 +1,2 @@
+cp ../../../COM_2021_10_20_injection_swapped_q7/Beam2/beta_beat_before_after_pre_beam1.pdf beam1
+cp ../../../COM_2021_10_20_injection_swapped_q7/Beam2/beta_beat_before_after_pre_beam2.pdf beam2
